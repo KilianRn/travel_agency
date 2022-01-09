8bad7b4ed0e9c17baee7d464e394ad89dd8db53e
KilianRn <kilian.rannou@gmail.com>
2022-01-09 15:49:56 +0100

    Partie 3 - L’API de prédiction

---
+++ prediction-engine/src/main/java/fr/lernejo/prediction/TempCountryController.java
@@ -0,0 +1,39 @@
+package fr.lernejo.prediction;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+@RestController
+public class TempCountryController {
+
+    @GetMapping("/api/temperature")
+    public final Object TempCountry(@RequestParam String country){
+
+            final TemperatureService temperatureService = new TemperatureService();
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            //System.out.println("Aujourd'hui, nous sommes le: "+sdf.format(cal.getTime()));
+            ArrayList<TempCountryDay> listTempCountry = new ArrayList<>();
+            try {
+                cal.add(Calendar.DATE, -1);
+            listTempCountry.add(new TempCountryDay(sdf.format(cal.getTime()), temperatureService.getTemperature(country)));
+            cal.add(Calendar.DATE, -1);
+            listTempCountry.add(new TempCountryDay(sdf.format(cal.getTime()), temperatureService.getTemperature(country)));
+            return listTempCountry;
+
+        }catch (UnknownCountryException error){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Country doesn't exist... ERROR HTTP 417");
+        }
+
+        }
+    }
---
+++ prediction-engine/src/main/java/fr/lernejo/prediction/TempCountryDay.java
@@ -0,0 +1,4 @@
+package fr.lernejo.prediction;
+
+public record TempCountryDay(String date, double temperature) {
+}
